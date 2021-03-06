6cd2b659b0305fca53e42270e71198c123876ef4
Artem Kulyhin <akulyhin@gmail.com>
2021-03-06 22:23:57 +0200

    Git Innit

--- src/js/magnific-popup.js
+++ src/js/magnific-popup.js
@@ -23,35 +23,24 @@ $('.design-content_img').magnificPopup({
   });
 
   $('.promo').magnificPopup({
-    type:'iframe',
+    type: 'iframe',
     iframe: {
-        markup: '<div class="mfp-iframe-scaler">'+
-        '<div class="mfp-close"></div>'+
-        '<iframe id="player" class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
-        '</div>', // HTML markup of popup, 'mfp-close' will be replaced by the close button
-        patterns: {
-            youtube: {
-                index: 'youtube.com/',
-                id: 'v=',
-                src: '//www.youtube.com/embed/%id%?rel=0&autoplay=1&enablejsapi=1'
-            }
-        },
-        srcAction: 'iframe_src',
-    },
-    callbacks: {
-      open: function () {
-        new YT.Player('player', {
-          events: {
-            'onStateChange': onPlayerStateChange
-           }
-        });
+    patterns: {
+      youtube: {
+        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
+        id: 'v=', // String that splits URL in a two parts, second part should be %id%
+        // Or null - full URL will be returned
+        // Or a function that should return %id%, for example:
+        // id: function(url) { return 'parsed id'; }
+        src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0' // Урл, который берется из кода iframe
       }
+    }
     },
     midClick: true,
     closeOnBgClick: false,
     removalDelay: 500,
     mainClass: 'mfp-fade mfp-video',
-});
+    });
 
 
   
