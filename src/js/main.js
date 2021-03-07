d2a19a52b5bcf52d82795a583d4984fc527ec67a
Artem Kulyhin <akulyhin@gmail.com>
2021-03-07 18:39:26 +0200

    Last updates

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+// Mobile menu
+$('.mobile-menu_button').click(function() {
+    $('.overlay-menu').toggleClass('active-menu');
+  });
+  
+  $('.close-menu').click(function() {
+    $('.overlay-menu').toggleClass('active-menu');
+  });
+  
+  $('.mobile-menu_link').click(function() {
+    $('.overlay-menu').toggleClass('active-menu');
+  });
+  // Mobile Menu
