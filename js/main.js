6a1674df2ee6fa70d9205ceb83a87c9c7da2a35c
Artem Kulyhin <akulyhin@gmail.com>
2021-03-07 19:30:16 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,1 @@
+"use strict";$(".mobile-menu_button").click(function(){$(".overlay-menu").toggleClass("active-menu")}),$(".close-menu").click(function(){$(".overlay-menu").toggleClass("active-menu")}),$(".mobile-menu_link").click(function(){$(".overlay-menu").toggleClass("active-menu")});
