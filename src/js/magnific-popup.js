24993bcd52bb54cfa05f002f80d17ff4d54b062f
Artem Kulyhin <akulyhin@gmail.com>
2021-03-08 23:25:10 +0200

    Last updates

--- src/js/magnific-popup.js
+++ src/js/magnific-popup.js
@@ -22,25 +22,22 @@ $('.design-content_img').magnificPopup({
     
   });
 
-  $('.promo').magnificPopup({
-    type: 'iframe',
-    iframe: {
-    patterns: {
-      youtube: {
-        index: 'youtube.com/', // String that detects type of video (in this case YouTube). Simply via url.indexOf(index).
-        id: 'v=', // String that splits URL in a two parts, second part should be %id%
-        // Or null - full URL will be returned
-        // Or a function that should return %id%, for example:
-        // id: function(url) { return 'parsed id'; }
-        src: '//www.youtube.com/embed/%id%?autoplay=1&rel=0' // Урл, который берется из кода iframe
-      }
-    }
-    },
-    midClick: true,
-    closeOnBgClick: false,
-    removalDelay: 500,
-    mainClass: 'mfp-fade mfp-video',
+    $('.promo').magnificPopup({
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+        fixedContentPos: false,
+        iframe: {
+            markup: '<div class="mfp-iframe-scaler">'+
+                    '<div class="mfp-close"></div>'+
+                    '<iframe class="mfp-iframe" frameborder="0" allowfullscreen autoplay="1"></iframe>'+
+                '</div>',
+
+            srcAction: 'iframe_src',
+            }
     });
+
 
 
     $('.modalForm').magnificPopup({
